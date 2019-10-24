914bdc678f378b3f594492a67faeb5d3fcda1ee7
Vincent2 <Vincent_tu@yahoo.com>
2019-10-23 18:20:12 -0700

    Edit calendar.c
    Implement functions to print out calendar properly, and still need to calculate the day 0 of a given year. Also, still trying to fix the 9 in the calendar as it is not aligning with the other numbers as it should be.

--- calendar.c
+++ calendar.c
@@ -3,13 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TILDA '~'
+#define TWO 2
+#define MAX 63
+#define DAYSOFWEEK 7
+#define NINE 9
+
+void printTilda(int total)
+{
+	for(int i = 0; i < total; i++)
+	{
+		printf("%c", TILDA);
+	}
+}
 
 int main()
 {
 	char *months[] = {" ", "Janurary", "Feburary", "March", "April", "June",
 	"July", "August", "September", "October", "Novemeber", "Decemeber"};
-	char *days[] = {" ", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-"Friday", "Saturday"};
+	char *days[] = {" ", "Sun", "Mon", "Tue", "Wed", "Thu","Fri", "Sat"};
+  int DaysOfMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	printf("Enter a year you wish to view the calendar for: ");
 	int User_Year;
 	scanf("%d", &User_Year);
@@ -17,11 +30,51 @@ int main()
 
 	for(int month = 1; month < 12; month++)
 	{
-		printf("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~%s~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n", 
-		months[month]);
+		int firsthalf, secondhalf;
+		//totale atilda characaters is 62
+		int length = strlen(months[month]);
+		int total = MAX - length;
+
+		if(total % TWO != 0 )
+		{
+			firsthalf = (total / 2) + 1;
+		}else
+		{
+			firsthalf = (total / 2);
+		}
+		secondhalf = total / 2;
+
+		printf("\n\n");
+		printTilda(firsthalf);
+		printf("%s", months[month]);
+		printTilda(secondhalf);
+		printf("\n");
+
 		for(int day = 1; day <=7; day++)
 		{
-			printf(" %s ", days[day]);
+			printf("%s       ", days[day]);
+		}
+		printf("\n");
+
+		for(int days = 1; days <= DaysOfMonth[month]; days++)
+		{
+			if(days % DAYSOFWEEK == 1)
+			{
+				printf(" %d", days);
+			}else
+			{
+			if(days == NINE)
+			{
+				printf("%11d", days);
+			}else
+			{
+			printf("%10d", days);
+		  }
+			}
+			if(days% DAYSOFWEEK == 0)
+			{
+				printf("\n");
+			}
 		}
 		printf("\n");
 	}
